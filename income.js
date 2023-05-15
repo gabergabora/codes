e109720553ee84573d0fc1b27b4fa7cba998e4f4
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-05-15 23:56:18 +0300

    Create income.js

---
+++ income.js
@@ -0,0 +1,28 @@
+// GET MONTHLY INCOME
+
+router.get("/income", isAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).send(income);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
